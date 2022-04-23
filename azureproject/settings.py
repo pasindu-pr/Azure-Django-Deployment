7ed25ac7791254fa6e4a278f1eced149245184fd
Pasindu Prabhashitha <pasinduprabashitha@gmail.com>
2022-04-23 17:26:46 +0530

    Change some config

--- azureproject/settings.py
+++ azureproject/settings.py
@@ -94,7 +94,7 @@ DATABASES = {
         'NAME': os.environ['DBNAME'],
         'HOST': os.environ['DBHOST'],
         'USER': os.environ['DBUSER'],
-        'PASSWORD': os.environ['DBPASS'] 
+        'PASSWORD': os.environ['DBPASS']
     }
 }
 
